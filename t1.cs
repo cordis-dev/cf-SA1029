329856dbf9536b4f4c110e52906f74bb103abe0e
cordis-dev <cordis.dev@gmail.com>
2016-09-12 16:03:38 +0300

    Create t1.cs

---
+++ t1.cs
@@ -0,0 +1,9 @@
+    public class t1
+    {
+        public string GetText(string input)
+        {
+            return input?
+                .Replace("1", "2")
+                .Replace("3", "4");
+        }
+    }
